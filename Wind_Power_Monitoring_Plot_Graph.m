c45faec4e4eef9896eb4605f41f1ec1016d2dbf8
Jessie Lim Zhi Min <85476985+jessielimzhimin@users.noreply.github.com>
2022-07-01 09:33:40 +0800

    Add files via upload

---
+++ Wind_Power_Monitoring_Plot_Graph.m
@@ -0,0 +1,16 @@
+readChId = 1785844 %Write your own channel ID
+readKey = 'Z4G68PPLV1JD0IKA'; %Write your own read key (not write key!)
+
+%collect the number of points from the channel
+[windPower,timeStamps] = thingSpeakRead(readChId,'fields',[1,2,3,4,5,6],...
+    'NumPoints',150,'ReadKey',readKey);
+
+
+%Plot the graph
+plot(timeStamps,dewPointData);
+xlabel('TimeStamps');
+ylabel('Measured Values');
+title('Wind Power Measurement');
+legend({'Wind Speed','Wind Power Density','Wind Power', ...
+    'Air Density', 'Temperature', 'Pressure'});
+grid on;
